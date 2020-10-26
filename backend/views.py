346c4a4b02a536eb0ec921284e375f223669787d
cajmera <cajmera@umich.edu>
2020-10-25 21:03:25 -0400

    update

--- backend/views.py
+++ backend/views.py
@@ -37,6 +37,15 @@ class CredentialsCreate(views.APIView):
             return response.Response(serializer.data, status=status.HTTP_201_CREATED)
         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class TeamsListCreate(generics.ListCreateAPIView):
-    queryset = Teams.objects.all()
-    serializer_class = TeamsSerializer
+class TeamsListCreate(views.APIView):
+    # queryset = Teams.objects.all()
+    # serializer_class = TeamsSerializer
+    
+    def post(self, request):
+
+        # post to table
+        serializer = TeamsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+        return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
