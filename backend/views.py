cd7c08dfbad06533f4d85a04fe44bbb9c2e29896
Yixin Xiao <yixinxiao7@gmail.com>
2020-10-29 17:45:08 -0400

    get request for all teams

--- backend/views.py
+++ backend/views.py
@@ -38,9 +38,23 @@ class CredentialsCreate(views.APIView):
         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class TeamsListCreate(views.APIView):
-    # queryset = Teams.objects.all()
-    # serializer_class = TeamsSerializer
     
+    def get(self, request):
+        teams_info = [[team.team_name, team.team_leader, team.team_info, team.team_progress, team.team_picture]
+                        for team in Teams.objects.all()]
+        all_team_data = []
+        for team in teams_info:
+            data = {
+                    "team_name": team[0],
+                    "team_leader": team[1],
+                    "team_info": team[2],
+                    "team_progress": team[3],
+                    "team_picture": team[4]
+                    }
+            all_team_data.append(data)
+            
+        return response.Response(all_team_data, status=status.HTTP_200_OK)
+
     def post(self, request):
 
         # post to table
