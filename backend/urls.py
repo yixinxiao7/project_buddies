5bdebd6e106097cea080ad0350393a665dd5c52e
Yixin Xiao <yixinxiao7@gmail.com>
2020-11-14 00:53:17 -0500

    new persons get endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/person/', views.PersonListCreate.as_view()),
+    path('api/person/', views.PersonView.as_view()),
     path('api/credentials/', views.CredentialsCreate.as_view()),
     path('api/teams/', views.TeamsListCreate.as_view()),
     path('api/project/', views.ProjectListCreate.as_view()),
